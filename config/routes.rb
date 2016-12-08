0d945fd36dc687c7e13cf563ad9f959f9f2eb4e6
Zach Nusbaum <nusbaumz@msu.edu>
2016-12-08 15:00:43 -0700

    rails_apps_composer: extras

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :users
-
     root to: "users#index"
   end
-
   root to: 'visitors#index'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
